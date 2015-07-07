a66589282d4ea2be4d112e909c4b47978d8d9d23
Guillaume Klein <guillaume.klein4@gmail.com>
2015-07-07 20:01:01 +0200

    Proper command line usage

--- src/tfidf/main.cc
+++ src/tfidf/main.cc
@@ -1,20 +1,25 @@
 #include <iostream>
+#include <fstream>
 
 #include "filesystem.hh"
 
 int main(int argc, char* argv[])
 {
-  if (argc != 2)
+  if (argc != 3)
   {
+    std::cerr << "usage: " << argv[0] << " /path/to/doc /path/to/repository" << std::endl;
     return 1;
   }
 
   TfIdf tfidf;
-  Filesystem fs(argv[1], true);
+  Filesystem fs(argv[2], true);
   fs.fetch(tfidf);
   tfidf.compute_tfidf();
 
-  for (const ResultDocument& res: tfidf.ranked_similarity("life learning", 5))
+  std::ifstream ifs(argv[1]);
+  std::string content((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
+
+  for (const ResultDocument& res: tfidf.ranked_similarity(content, 5))
     std::cout << res << std::endl;
 
   return 0;
