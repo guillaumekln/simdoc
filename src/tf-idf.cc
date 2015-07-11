82f23013eafc546285ca2344d904d263d37a79c2
Guillaume Klein <guillaume.klein4@gmail.com>
2015-07-11 18:17:47 +0200

    Parallelize TF-IDF computation

--- src/tf-idf.cc
+++ src/tf-idf.cc
@@ -15,10 +15,14 @@ void TfIdf::compute_tfidf()
 {
   _idf.map([&] (size_t, double freq) { return 1 + log(_documents.size() / freq); });
 
-  for (DocumentFrequency& doc: _documents)
-  {
-    doc.compute_tfidf(_idf);
-  }
+  tbb::parallel_for(tbb::blocked_range<size_t>(0, _documents.size()),
+                    [&](const tbb::blocked_range<size_t>& r)
+                    {
+                      for (size_t i = r.begin(); i != r.end(); ++i)
+                      {
+                        _documents[i].compute_tfidf(_idf);
+                      }
+                    });
 }
 
 void TfIdf::compute_similarity(std::vector<ResultDocument>& res, size_t max_results) const
