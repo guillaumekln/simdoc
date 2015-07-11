b17ab645431af94e085bd664255cc93daa93b5fb
Guillaume Klein <guillaume.klein4@gmail.com>
2015-07-11 21:29:08 +0200

    Parallelize documents processing

--- src/filesystem.cc
+++ src/filesystem.cc
@@ -4,6 +4,9 @@
 #include <stdexcept>
 #include <fstream>
 
+#include <tbb/parallel_for.h>
+#include <tbb/blocked_range.h>
+
 Filesystem::Filesystem(const std::string& directory, bool recursive)
   : _directory(directory)
   , _recursive(recursive)
@@ -14,7 +17,7 @@ Filesystem::Filesystem(const std::string& directory, bool recursive)
   }
 }
 
-void Filesystem::fetch_rec(const boost::filesystem::path& dir, TfIdf& tfidf)
+void Filesystem::fetch_rec(const boost::filesystem::path& dir)
 {
   boost::filesystem::directory_iterator end_it;
 
@@ -22,21 +25,42 @@ void Filesystem::fetch_rec(const boost::filesystem::path& dir, TfIdf& tfidf)
   {
     if (_recursive && boost::filesystem::is_directory(it->status()))
     {
-      fetch_rec(it->path(), tfidf);
+      fetch_rec(it->path());
     }
     else if (boost::filesystem::is_regular_file(it->status()))
     {
-      std::string path = it->path().native();
-      std::string filename = it->path().filename().native();
-
-      std::ifstream ifs(path);
-      std::string content((std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>()));
-      tfidf.add(filename, content);
+      _files.push_back(it->path());
     }
   }
 }
 
+void Filesystem::merge(TfIdf& main, const TfIdf& aux)
+{
+  std::lock_guard<std::mutex> lock(_mutex);
+  main.merge(aux);
+}
+
 void Filesystem::fetch(TfIdf& tfidf)
 {
-  fetch_rec(_directory, tfidf);
+  fetch_rec(_directory);
+
+  tbb::parallel_for(tbb::blocked_range<size_t>(0, _files.size()),
+                    [&](const tbb::blocked_range<size_t>& r)
+                    {
+                      TfIdf aux;
+
+                      for (size_t i = r.begin(); i != r.end(); ++i)
+                      {
+                        std::string path = _files[i].native();
+                        std::string filename = _files[i].filename().native();
+
+                        std::ifstream ifs(path);
+                        std::string content((std::istreambuf_iterator<char>(ifs)),
+                                            (std::istreambuf_iterator<char>()));
+
+                        aux.add(filename, content);
+                      }
+
+                      merge(tfidf, aux);
+                    });
 }
--- src/filesystem.hh
+++ src/filesystem.hh
@@ -3,6 +3,8 @@
 
 # include <boost/filesystem.hpp>
 # include <string>
+# include <vector>
+# include <mutex>
 
 # include "data-source.hh"
 
@@ -10,12 +12,15 @@ class Filesystem: public DataSource
 {
 public:
   Filesystem(const std::string& directory, bool recursive = false);
+  void merge(TfIdf& main, const TfIdf& aux);
 
   virtual void fetch(TfIdf& tfidf) override;
 
 private:
-  void fetch_rec(const boost::filesystem::path& dir, TfIdf& tfidf);
+  void fetch_rec(const boost::filesystem::path& dir);
 
+  std::vector<boost::filesystem::path> _files;
+  std::mutex _mutex;
   boost::filesystem::path _directory;
   bool _recursive;
 };
