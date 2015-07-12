b21c68ec4755f396569f4dc77c0c1356b9341804
Guillaume Klein <guillaume.klein4@gmail.com>
2015-07-12 10:38:23 +0200

    Use Boost program options

--- src/main.cc
+++ src/main.cc
@@ -2,50 +2,53 @@
 #include <iostream>
 #include <string>
 
+#include <boost/program_options.hpp>
 #include <tbb/task_scheduler_init.h>
 
 #include "filesystem.hh"
 #include "timer.hh"
 
+namespace po = boost::program_options;
+
 int main(int argc, char* argv[])
 {
-  if (argc != 4)
+  po::options_description desc("Allowed options");
+
+  desc.add_options()
+    ("help,h", "Display this help")
+    ("threads,t", po::value<unsigned>()->default_value(4), "Set the number of threads to use")
+    ("count,c", po::value<unsigned>()->default_value(5), "Set the number of similar documents to list")
+    ("dir,d", po::value<std::string>(), "Set the directory to process")
+    ("recursive,r", po::bool_switch()->default_value(false), "Enable a recursive directory traversal")
+    ;
+
+  po::positional_options_description p;
+  p.add("dir", -1);
+
+  po::variables_map vm;
+  po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), vm);
+  po::notify(vm);
+
+  if (vm.count("help"))
   {
-    std::cerr << "usage: " << argv[0]
-              << " nb_threads nb_similar_docs /path/to/repository" << std::endl;
+    std::cerr << "Usage: " << argv[0] << " [options] directory" << std::endl
+              << desc;
+    return 0;
+  }
+
+  if (!vm.count("dir"))
+  {
+    std::cerr << "error: missing directory to process." << std::endl;
     return 1;
   }
 
-  unsigned long nb_threads;
-  unsigned long nb_sim;
-
-  try
-  {
-    nb_threads = std::stoul(argv[1], 0, 10);
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << "error: invalid thread count." << std::endl;
-    return 1;
-  }
-
-  try
-  {
-    nb_sim = std::stoul(argv[2], 0, 10);
-  }
-  catch (std::exception& e)
-  {
-    std::cerr << "error: invalid similar documents count." << std::endl;
-    return 1;
-  }
-
-  tbb::task_scheduler_init init(nb_threads);
+  tbb::task_scheduler_init init(vm["threads"].as<unsigned>());
   std::vector<ResultDocument> res;
   TfIdf tfidf;
 
   try
   {
-    Filesystem fs(argv[3], true);
+    Filesystem fs(vm["dir"].as<std::string>(), vm["recursive"].as<bool>());
 
     {
       Timer timer("Process documents");
@@ -65,7 +68,7 @@ int main(int argc, char* argv[])
 
   {
     Timer timer("Compute similarity");
-    tfidf.compute_similarity(res, nb_sim);
+    tfidf.compute_similarity(res, vm["count"].as<unsigned>());
   }
 
   std::sort(res.begin(), res.end());
